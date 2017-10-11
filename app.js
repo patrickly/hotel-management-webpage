46f2d5603fd9b6a0932b4164107cb7fba669e4f0
patrickly <patrickkimly@gmail.com>
2017-10-11 05:05:34 -0700

    changed engine form html to pug in app.js

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ var cons = require('consolidate');
 app.engine('html',cons.swig);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','html');
+//app.set('view engine','html');
+app.set('view engine','pug');
 //app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
